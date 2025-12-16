1d9c7cb5d20869b43affb340e8428f3ed0b54b74
MiguelSosa115 <100496793@alumnos.uc3m.es>
2025-12-16 19:04:09 +0100

    Add files via upload

---
+++ PID Curves/plots.m
@@ -0,0 +1,34 @@
+% BaseFigure.m
+% Reads Base.csv with #! header lines and plots the numeric data
+
+clear; clc;
+
+filename = 'Link1 negativo.csv';
+
+% Detect import options
+opts = detectImportOptions(filename);
+
+% The header lines starting with "#!" are not real data,
+% so we tell MATLAB to treat them as comments.
+opts.CommentStyle = '#!';
+
+% Now read the data
+data = readmatrix(filename, opts);
+
+% Remove any missing rows caused by blank lines
+data = data(~isnan(data));
+
+% Plot the data
+figure;
+plot(data, 'LineWidth', 2);
+grid on;
+
+title('Link2.csv Data Plot');
+xlabel('Index');
+ylabel('Pulses');
+
+% Basic stats
+fprintf('Loaded %d data points.\n', length(data));
+fprintf('Min value: %g\n', min(data));
+fprintf('Max value: %g\n', max(data));
+fprintf('Mean value: %g\n', mean(data));
